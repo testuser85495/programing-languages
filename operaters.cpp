dc500f59cc94b9bb8756114674b3303bca1e263e
testuser85495 <testuser8545@gmail.com>
2023-11-09 13:23:23 +0530

    first commit

---
+++ operaters.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("enter your number :");
+	scanf("%d %d ",&a,&b);
+	
+	printf("your result is:%d \n",a+b);
+	printf("your result is:%d \n",a*b);
+	printf("your result is:%d \n",a/b);
+	printf("your result is:%d \n",a-b);
+
+	return 0;
+	
+}
